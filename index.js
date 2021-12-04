d14a075c289b9d8390d9e69475edf889986eb7d0
icycoba <icycoba@gmail.com>
2021-12-04 18:07:51 +0100

    Propojeni frontendu s backendem

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const services = [
 
 app.use(express.json())
 
+app.use(function (req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+})
+
 app.listen(
     PORT, () => console.log(`Running on http://localhost:${PORT}`)
 )
